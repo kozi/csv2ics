a7cde213954189ae7f51c662574fe2ea00a1733d
kozi <martin@kozianka.de>
2016-10-21 11:50:16 +0200

    Open external links in new tab

--- templates/layout.php
+++ templates/layout.php
@@ -19,6 +19,14 @@
     <footer>
         <h1><?=$this->e($copyright)?></h1>
     </footer>
+    
+    <script>
+        // open external links in new tab
+        var i, links = document.links, length = links.length;
+        for(i=0;i<length;i++) {
+            (links[i].hostname==window.location.hostname) || links[i].setAttribute("target", "_blank"); 
+        }
+    </script>
 </body>
 </html>
 
